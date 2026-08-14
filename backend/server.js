428dcde73e76e7b7e149740176b599876ae12944
jigyash <realjigyash19@gmail.com>
2026-08-14 19:41:31 +0530

    backend

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+import app from "./src/app.js"
+app.get("/",(req,res)=>{
+    res.json({
+        message:"ShardFS server is running "
+    })
+})
+app.listen(3000,()=>{
+    console.log("server running on http://localhost:3000")
+})
---
+++ backend/src/app.js
@@ -0,0 +1,6 @@
+import express from 'express'
+import morgan from 'morgan'
+const app=express();
+app.use(express.json());
+app.use(morgan('dev'))//logger
+export default app;
