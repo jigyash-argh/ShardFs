9c3a37e34a5832a1736d703806eb53372d05bc43
jigyash <realjigyash19@gmail.com>
2026-08-15 10:21:34 +0530

    done

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,4 +1,17 @@
 import userModel from "../models/user.model.js"
+import crypto from 'crypto'
 async function register(req,res){
     const {username,email,password}=req.body
+    const isAlreadyRegistered= await userModel.findOne({
+        $or:[
+            {username},
+            {email}
+        ]
+    })
+    if(isAlreadyRegistered){
+        req.status(400).json({
+            message:"username or email already exists"
+        })
+    }
+    const HashedPassword=crypto.createHash("sha256").update(password).digest("hex");
 }
